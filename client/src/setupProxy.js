1c5e4abe65bab8da7c55a9c799a51b419358d293
Xingjian Guo <353583240@qq.com>
2020-02-03 21:33:06 +1100

    new frontend

---
+++ client/src/setupProxy.js
@@ -0,0 +1,8 @@
+// /client/src/setupProxy.js
+
+const proxy = require('http-proxy-middleware');
+
+module.exports = function(app) {
+    //proxy setup currently at localhost 5000
+    app.use(proxy('/api/*', { target: 'http://localhost:5000' }))
+};
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-//  index.js
+//  index.js at root
 
 const express = require('express');
 const mongoose = require('mongoose');
@@ -15,3 +15,14 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`app running on port ${PORT}`)
 });
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  const path = require('path');
+  app.get('*', (req,res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+
+}
+
